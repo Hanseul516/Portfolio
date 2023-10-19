e8a106d1873017294ab7019c9abe32a6067e70ad
최한슬 <hanseulo@naver.com>
2023-10-19 23:49:03 +0900

    portfolio

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -18,8 +18,15 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}>
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]" />
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem]
+          rounded-full blur-[10em]
+          sm:w:[68,75em]
+          md:left:[33,75em]
+          lg:left:[28,75em]
+          xl:left:[15em]
+          xl:left:[5em]"
+        />
         <Header />
         {children}
       </body>
