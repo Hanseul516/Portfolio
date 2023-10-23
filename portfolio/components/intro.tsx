782d8a309afd4be9a6cf38f7166fe8b2a2e6e926
최한슬 <hanseulo@naver.com>
2023-10-24 06:27:57 +0900

    portfolio

--- portfolio/components/intro.tsx
+++ portfolio/components/intro.tsx
@@ -3,15 +3,21 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight } from "react-icons/bs";
 
 export default function Intro() {
   return (
-    <section>
+    <section className="nb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
           >
             <Image
               src="https://images.unsplash.com/photo-1519750783826-e2420f4d687f?auto=format&fit=crop&q=80&w=774&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -22,9 +28,42 @@ export default function Intro() {
               priority={true}
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
-            <span className="absolute bottom-0 right-0 text-4xl">👋🏻</span>
           </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋🏻
+          </motion.span>
         </div>
+      </div>
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">안녕하세요, 최한슬입니다.</span>
+        <br></br>간호사에서 <span className="font-bold">프론트엔드 개발자</span>
+        로의 여정을 시작합니다.
+        {/* <span className="font-bold">8 years</span> of experience. I enjoy
+        building <span className="italic">React</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>. */}
+      </motion.h1>
+
+      <div>
+        <Link href="#contact">
+          Contact me here
+          <BsArrowRight />
+          {""}
+        </Link>
       </div>
     </section>
   );
